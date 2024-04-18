232a7d29a5b03ad0aed9bea6dd9276d82211458f
Andrew Lobban <andrewglobban@gmail.com>
2024-04-18 16:02:45 -0500

    Add styles to Fallback text

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -10,8 +10,13 @@ class PlacesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (places.isEmpty) {
-      return const Center(
-        child: Text('No places added yet'),
+      return Center(
+        child: Text(
+          'No places added yet',
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
       );
     }
 
