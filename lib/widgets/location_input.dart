6a73c459e0cbe6678ba8995adf67e3272ec3e353
Andrew Lobban <andrewglobban@gmail.com>
2024-04-30 13:37:48 -0500

    Implement image of map

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -19,6 +19,15 @@ class _LocationInputState extends State<LocationInput> {
   PlaceLocation? _pickedLocation;
   var _isGettingLocation = false;
 
+  String get locationImage {
+    if (_pickedLocation == null) {
+      return '';
+    }
+    final lat = _pickedLocation!.latitude;
+    final lng = _pickedLocation!.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:S%7C$lat,$lng&key=AIzaSyCCA4iGpDlVQ9gl-fmA4_w8JF8hOxFT5ug';
+  }
+
   void _getCurrentLocation() async {
     Location location = Location();
 
@@ -80,6 +89,15 @@ class _LocationInputState extends State<LocationInput> {
             color: Theme.of(context).colorScheme.onBackground,
           ),
     );
+
+    if (_pickedLocation != null) {
+      previewContent = Image.network(
+        locationImage,
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      );
+    }
 
     if (_isGettingLocation) {
       previewContent = const CircularProgressIndicator();
